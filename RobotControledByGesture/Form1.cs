e4198db823ebb287cc94fba6936836641d1f369a
NarielNTB <NarielNtbDESKTOP-625NDPG>
2017-03-30 11:41:11 +0200

    pridani tlačitka

--- RobotControledByGesture/Form1.cs
+++ RobotControledByGesture/Form1.cs
@@ -7,24 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RobotControledByGesture.Lego;
 
 namespace RobotControledByGesture
 {
     public partial class Form1 : Form
     {
+       
+        Robot R;
         public Form1()
         {
+
             InitializeComponent();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            R  = new Robot();
+            R.InitializeRobotAsync("COM8");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+                R.ForwardAsync();
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            R.StopMoving();
         }
     }
 }
---
+++ RobotControledByGesture/Lego/Robot.cs
@@ -0,0 +1,76 @@
+﻿using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotControledByGesture.Lego
+
+{
+    class Robot 
+    {
+       
+        private Brick brick;
+
+        public Robot()
+        {
+            
+        }
+        
+        public async void InitializeRobotAsync(string COMPort)
+        {
+            brick = new Brick(new BluetoothCommunication(COMPort));
+            await brick.ConnectAsync();
+        }
+    
+        public async void StopMoving()
+        {
+            await brick.DirectCommand.PlayToneAsync(10, 100, 200);
+         //   await brick.DirectCommand.StopMotorAsync(OutputPort.All, true);
+            Debug.WriteLine("Robot stops moving.");
+        }
+
+
+        public async void LeftAsync()
+        {
+            //await brick.DirectCommand.StartMotorAsync(OutputPort.All);
+            //await brick.DirectCommand.TurnMotorAtPowerAsync(OutputPort.B, -40);
+            //await brick.DirectCommand.TurnMotorAtPowerAsync(OutputPort.A, +40);
+        
+            await brick.DirectCommand.TurnMotorAtSpeedForTimeAsync(OutputPort.B, -30, 500, false);
+            await brick.DirectCommand.TurnMotorAtSpeedForTimeAsync(OutputPort.A, +30, 500, false);
+
+            Debug.WriteLine("Robot goes left.");
+        }
+
+        public async void RightAsync()
+        {
+            //await brick.DirectCommand.StartMotorAsync(OutputPort.All);
+            //await brick.DirectCommand.TurnMotorAtPowerAsync(OutputPort.A, -40);
+            //await brick.DirectCommand.TurnMotorAtPowerAsync(OutputPort.B, +40);
+            await brick.DirectCommand.TurnMotorAtSpeedForTimeAsync(OutputPort.A, -30, 500, false);
+            await brick.DirectCommand.TurnMotorAtSpeedForTimeAsync(OutputPort.B, +30, 500, false);
+            Debug.WriteLine("Robot goes right.");
+        }
+
+        public async void ForwardAsync()
+        {
+            await brick.DirectCommand.PlayToneAsync(10, 500, 200);
+
+            // await brick.DirectCommand.StartMotorAsync(OutputPort.All);
+            // await brick.DirectCommand.TurnMotorAtPowerAsync(OutputPort.All, 40);
+         //   await brick.DirectCommand.TurnMotorAtSpeedForTimeAsync(OutputPort.All, 40, 500, false);
+            Debug.WriteLine("Robot goes forward.");
+       }
+
+        public void AbortCommands()
+        {
+            StopMoving();
+            brick.Disconnect();
+        }
+    }
+}

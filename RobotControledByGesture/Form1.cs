9a0369fd0da811787a22926c28a568a116c4dbd7
Fill <P16108@student.osu.cz>
2017-04-15 14:20:22 +0200

    webcam capture

--- RobotControledByGesture/Form1.cs
+++ RobotControledByGesture/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using RobotControledByGesture.Lego;
+using AForge.Video;
+using AForge.Video.DirectShow;
 
 namespace RobotControledByGesture
 {
@@ -22,10 +24,20 @@ namespace RobotControledByGesture
 
         }
 
+        private FilterInfoCollection webcam;
+        private VideoCaptureDevice cam;
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
             R  = new Robot();
             R.InitializeRobotAsync("COM8");
+
+            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo VideoCaptureDevice in webcam)
+            {
+                comboBox1.Items.Add(VideoCaptureDevice.Name);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -37,5 +49,27 @@ namespace RobotControledByGesture
         {
             R.StopMoving();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cam = new VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString);
+            cam.NewFrame += new NewFrameEventHandler(Cam_NewFrame);
+            cam.Start();
+        }
+
+        void Cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bit = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bit;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (cam.IsRunning)
+            {
+                cam.Stop();
+            }
+        }
+
     }
 }
